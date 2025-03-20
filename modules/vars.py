869ac1f525860cb88b62a0079bfee90f104ca33f
BharatChhimpa <bhartlal2013@gmail.com>
2025-03-20 18:48:38 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
-api_id = "29683927"
-api_hash = "b2eb32ac1030edd8bc36c7b554ef6fc3"
-bot_token = "7903348261:AAFgADxgmNQUiW9UhF_0srnTFCKWT6RLW90"
+api_id = "29883927"
+api_hash = "b2eb32ajkk30edd8bc36c7b554ef6fc3"
+bot_token = "7903348261:AAFgADxgmNnmkW9UhF_0srnTFCKWT6RLW90"
